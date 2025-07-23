f78b80d5dadb20a9580657aabc58b83516e085da
JagdeepSingh13 <jagdeeps3105@gmail.com>
2025-07-23 16:07:14 +0530

    websockets set-up

--- main.go
+++ main.go
@@ -5,7 +5,9 @@ import (
 	"log"
 	"time"
 
+	"github.com/gofiber/contrib/websocket"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"gopkg.in/mgo.v2/bson"
@@ -16,11 +18,31 @@ var quizCollection *mongo.Collection
 func main() {
 	setupDB()
 	app := fiber.New()
+	app.Use(cors.New())
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
 	app.Get("/api/quizzes", getQuizzes)
+	app.Get("/ws", websocket.New(func(c *websocket.Conn) {
+		var (
+			mt  int
+			msg []byte
+			err error
+		)
+		for {
+			if mt, msg, err = c.ReadMessage(); err != nil {
+				log.Println("read", err)
+				break
+			}
+			log.Printf("recv: %s", msg)
+
+			if err = c.WriteMessage(mt, msg); err != nil {
+				log.Println("write", err)
+				break
+			}
+		}
+	}))
 
 	log.Fatal(app.Listen(":3000"))
 }
